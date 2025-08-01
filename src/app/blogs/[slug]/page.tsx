2816ea9a1abdf62f92418f1dadc22140b4193f98
Phojiii <qasimbilal19@gmail.com>
2025-08-01 10:12:33 +0500

    Testing the Slug

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,30 +1,22 @@
+// @ts-nocheck
+
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import { notFound } from 'next/navigation';
 import { marked } from 'marked';
 
-type BlogPageProps = {
-  params: {
-    slug: string;
-  };
-};
-
 export async function generateStaticParams() {
   const files = fs.readdirSync(path.join(process.cwd(), 'src/content/blogs'));
-
   return files.map((file) => ({
     slug: file.replace('.md', ''),
   }));
 }
 
-// @ts-ignore
 export default async function Blog({ params }: { params: { slug: string } }) {
   const filePath = path.join(process.cwd(), 'src/content/blogs', `${params.slug}.md`);
 
-  if (!fs.existsSync(filePath)) {
-    return notFound();
-  }
+  if (!fs.existsSync(filePath)) return notFound();
 
   const fileContent = fs.readFileSync(filePath, 'utf-8');
   const { content, data } = matter(fileContent);
